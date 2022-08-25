4607cab29fe3fc5cdff4e31b01b70d2dedd7f98a
Zach Oda <zjodachow@gmail.com>
2022-08-25 15:07:14 -0400

    next update

--- models/index.js
+++ models/index.js
@@ -1,8 +1,9 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require("./Vote");
 
-// create associations
+// create associations between User & Post
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -11,5 +12,32 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+//Create associations between User & Post in relation to Vote
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id"
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id"
+});
+
+//Create direct associations between Post & Vote, User & Vote, Vote & User, Vote & Post
+Vote.belongsTo(User, {
+  foreignKey: "user_id"
+});
+Vote.belongsTo(Post, {
+  foreignKey: "post_id"
+});
+User.hasMany(Vote, {
+  foreignKey: "user_id"
+});
+Post.hasMany(Vote, {
+  foreignKey: "post_id"
+});
+
+module.exports = { User, Post, Vote };
 
