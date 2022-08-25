d83acfdeca6df84f0b2946357f47fdb1d7dad2cd
Zach Oda <zjodachow@gmail.com>
2022-08-25 13:41:05 -0400

    updated

--- config/connection.js
+++ config/connection.js
@@ -10,3 +10,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 module.exports = sequelize;
+
--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
+// import all models
+const Post = require('./Post');
 const User = require('./User');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
+
