3c2af0306b3c1a4df204bad3f9f26e9715865dbc
Zach Oda <zjodachow@gmail.com>
2022-09-04 14:16:09 -0400

    updated upvotes

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const homeRoutes = require("./home-routes");
-const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+
 router.use('/', homeRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
+router.use('/api', apiRoutes);
 
 module.exports = router;
